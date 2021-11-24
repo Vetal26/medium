1ec98a92e42a2c7c0466be57f65dbefccb0dccd5
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-11-24 18:32:37 +0300

    added styles and TopBar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalFeed from './pages/GlobalFeed/GlobalFeed';
 import Article from './pages/Article/Article';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
-    <div>
-      <h3>Welcome</h3>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<GlobalFeed />} />
-          <Route path="/articles/:slug" element={<Article />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<GlobalFeed />} />
+        <Route path="/articles/:slug" element={<Article />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

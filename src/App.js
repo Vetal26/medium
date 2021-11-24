6e536ec5572506f60906dd856c7f3a6a79362aa1
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-11-24 18:10:20 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalFeed from './pages/GlobalFeed/GlobalFeed';
+import Article from './pages/Article/Article';
 
 function App() {
-  return <div className="App">Hooks</div>;
+  return (
+    <div>
+      <h3>Welcome</h3>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GlobalFeed />} />
+          <Route path="/articles/:slug" element={<Article />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

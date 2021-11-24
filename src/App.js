6156f1768cc4735d33b1c29edf2f4a889ee9ca00
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-11-24 19:08:09 +0300

    login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalFeed from './pages/GlobalFeed/GlobalFeed';
 import Article from './pages/Article/Article';
 import TopBar from './components/TopBar';
+import Auth from './pages/Auth/Auth';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<GlobalFeed />} />
         <Route path="/articles/:slug" element={<Article />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/register" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );

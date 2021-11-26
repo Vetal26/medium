b01fa8611820c59e08ce239bb2e94fa49ab926ac
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-11-26 15:06:41 +0300

    added article page

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ErrorMessage from '../components/ErrorMessage';
+import Loading from '../components/Loading';
+import TagList from '../components/Taglist';
+import useFetch from '../hooks/useFetch';
 
 const Article = () => {
-  return <div>Article</div>;
+  const slug = useParams().slug;
+  const apiUrl = `/articles/${slug}`;
+  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
+  return (
+    <div className="article-page">
+      <div className="banner">
+        {!isLoading && response && (
+          <div className="container">
+            <h1>{response.article.title}</h1>
+            <div className="article-meta">
+              <Link to={`/profiles/${response.article.author.username}`}>
+                <img src={response.article.author.image} alt="" />
+              </Link>
+              <div className="info">
+                <Link to={`/profiles/${response.article.author.username}`}>
+                  {response.article.author.username}
+                </Link>
+                <span className="date">{response.article.createdAt}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="container page">
+        {isLoading && <Loading />}
+        {error && <ErrorMessage />}
+        {!isLoading && response && (
+          <div className="row article-content">
+            <div className="col-xs-12">
+              <div>
+                <p>{response.article.body}</p>
+              </div>
+              <TagList tags={response.article.tagList} />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Article;
